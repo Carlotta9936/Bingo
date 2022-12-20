fe58dd52921e262309b1a1bafca4427b7eb1e76d
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-12-20 09:53:22 +0100

    iniziato a fare la registrazione

--- bingo/src/app/app-routing.module.ts
+++ bingo/src/app/app-routing.module.ts
@@ -5,7 +5,12 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'registrazione',
+    loadChildren: () => import('./registrazione/registrazione.module').then( m => m.RegistrazionePageModule)
   }
+
 ];
 @NgModule({
   imports: [
