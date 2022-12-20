00aea9f8e6b386252baff1526120e9eb621e5c4e
Mr.Alsi <gabriel.alsina22@gmail.com>
2022-12-20 09:53:08 +0100

    Iniziato a fare la registrazione

---
+++ bingo/src/app/interfaces/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+    username: string;
+    password: string;
+    nome: string;
+    cognome: string;
+    mail: string;
+    crediti: number;
+}
--- bingo/src/environments/environment.ts
+++ bingo/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBcPzyJW_D9oIcwzVUN-Btt7M_nXGzS3wo",
+    authDomain: "bingo-bce96.firebaseapp.com",
+    projectId: "bingo-bce96",
+    storageBucket: "bingo-bce96.appspot.com",
+    messagingSenderId: "592520733548",
+    appId: "1:592520733548:web:f41ac5dd570ec33cfac949",
+    measurementId: "G-2DM0JRQTPY"
+  }
 };
 
 /*
