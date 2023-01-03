f5b880eddb26ce6e9da4c910aa635011dcbe9748
Mr.Alsi <gabriel.alsina22@gmail.com>
2023-01-03 16:58:29 +0100

    Guardia quando si accede all'app

--- bingo/src/app/app-routing.module.ts
+++ bingo/src/app/app-routing.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { GuardiaService } from './guardia.service';
 
 const routes: Routes = [
   {
+    path: 'first-page',
+    loadChildren: () => import('./first-page/first-page.module').then( m => m.FirstPagePageModule),
+    
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canActivate: [GuardiaService]
   },
   {
     path: 'registrazione',
     loadChildren: () => import('./registrazione/registrazione.module').then( m => m.RegistrazionePageModule)
-  },  {
+  },
+  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'crea-partita',
     loadChildren: () => import('./crea-partita/crea-partita.module').then( m => m.CreaPartitaPageModule)
-  }
+  },
+  
 
 
 ];
