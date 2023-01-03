16a525ba948dd41c8b0d49406f13e32c9aa1413b
Mr.Alsi <gabriel.alsina22@gmail.com>
2023-01-03 11:22:43 +0100

    Login

--- bingo/src/app/login/login.page.ts
+++ bingo/src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import { DatabaseService } from '../services/database.service';
 import { getDatabase, set, ref, onValue } from "firebase/database";
 import { response } from 'express';
 import { HttpClient } from '@angular/common/http';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginPage implements OnInit {
   db;
+  user?: User;
 
   constructor(public database: DatabaseService, public http: HttpClient) { 
     this.db = getDatabase();
@@ -20,25 +22,37 @@ export class LoginPage implements OnInit {
   }
 
   async login(value: any){
-    /*let url = await ref(this.db, 'users/'+ value.username);
-    let user = this.database.getUser(url).then((response: any) => {
-      console.log(response);
-    });*/
-
-    this.http.get('https://bingo-bce96-default-rtdb.europe-west1.firebasedatabase.app/Users/' + value.username).subscribe(data => {
-      console.log("D" + data);
-    })
-    //console.log("-->"  + user);
-    
-     
-    /*let user = await this.database.login(value.username, value.password).then((response) => {
-      if(response !== null){
-        if(response.password === value.password){
+    const user = ref(this.db, 'users/'+ value.username);
+    onValue(user, (snapshot) => {
+      try{
+        let u = snapshot.val();
+        if(u.password === value.password){
           console.log("Trovato");
+          this.user = u;
+        } else {
+          console.log("NOn trovato");
         }
+      } catch(e){
+        console.log("NOn trovato");
+      }
+    });
+
+/*
+    this.database.login(value.username, value.password).then((response: any) => {
+      console.log("Login");
+      console.log("RESPONSE: " + response);
+      if(response !== null){
+        //if(response.password === value.password){
+          console.log("Trovato");
+        //}
       } else {
         console.log("Non trovato");
       }
     });*/
+    //})
+
+
+      //.subscribe((response: User) => {
+        
   }
 }
--- bingo/src/app/services/database.service.ts
+++ bingo/src/app/services/database.service.ts
@@ -7,6 +7,7 @@ import { getDatabase, set, ref, onValue } from "firebase/database";
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import { PartitaData } from '../interfaces/PartitaData';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -33,6 +34,44 @@ export class DatabaseService {
     alert('user created');
   }
 
+  /*
+
+  login(username: string, password: string): Observable<User>{   
+    const user = ref(this.database, 'users/'+ username);
+      onValue(user, (snapshot) => {
+        //console.log("USer" + user)
+        return snapshot.val();
+    /*
+    return new Promise(resolve => {
+      const user = ref(this.database, 'users/'+ username);
+      onValue(user, (snapshot) => {
+        //console.log("USer" + user)
+        const u = snapshot.val();
+        try{
+          console.log(u)
+          if(u.password === password){
+            return u;
+          } 
+        } catch(e){
+        }
+        return null;
+      }); 
+      
+    })
+    
+
+  } 
+*/
+  getUser(user: any): any {
+    onValue(user, (snapshot) => {
+      console.log("User  " + user)
+      const u = snapshot.val();
+      console.log(u);
+      return u;
+    }); 
+  }
+
+  //Metodi per dati partita
   creaPartita(partita: PartitaData){
     set(ref(this.database, 'partita/'+partita.codice),{
       pubblica: partita.pubblica,
@@ -44,30 +83,6 @@ export class DatabaseService {
     alert('partita creata');
   }
 
-  async login(username: string, password: string): Promise<any>{    
-    const user = await ref(this.database, 'users/'+ username);
-
-    onValue(user, (snapshot) => {
-      console.log("USer" + user)
-      const u = snapshot.val();
-      console.log(u);
-      return u;
-    }); 
-
-  } 
-
-  getUser(user: any): any {
-    onValue(user, (snapshot) => {
-      console.log("User  " + user)
-      const u = snapshot.val();
-      console.log(u);
-      return u;
-    }); 
-  }
-
-  //Metodi per dati partita
-
-
   //Metodi per partita
   public aggiornaPartita(partita: Partita): void{
     set(ref(this.database, 'game/'+'AAA'), {
@@ -78,5 +93,13 @@ export class DatabaseService {
     })
   }
 
+
+  public ascoltaNumero(codicePartita: string): any {
+    const partita = ref(this.database, 'game/' + codicePartita)
+    onValue(partita, (snapshot) => {
+      const match = snapshot.val();
+      return match.ultimoNumero;
+    })
+  }
   
 }
